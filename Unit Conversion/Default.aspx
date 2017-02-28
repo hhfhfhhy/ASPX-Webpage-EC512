5af8f345e504526c7fd67ee9a2a998d97e1ea2c8
alex <hhfhfhhy@gmail.com>
2017-02-27 19:38:28 -0500

    Add files via upload

--- Unit Conversion/Default.aspx
+++ Unit Conversion/Default.aspx
@@ -1,1 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="default.aspx.cs" Inherits="_default" %>
 
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Units Coversion</title>
+    <style type="text/css">
+        .auto-style1 {
+            font-weight: normal;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1 style="font-size: xx-large; text-align: center"><span class="auto-style1"><strong>Units Coversion</strong></span><br />
+            <br />
+    
+        &nbsp;<asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+        <asp:DropDownList ID="dList1" runat="server"></asp:DropDownList>
+    
+    &nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox1_TextChanged" Height="25px"></asp:TextBox>
+        <asp:DropDownList ID="dList2" runat="server"> </asp:DropDownList>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Conversion" />
+    
+        </h1>
+        <asp:Label ID="Label1" runat="server"  Text=""></asp:Label>
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Unit Conversion/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _default : System.Web.UI.Page
+{
+
+    protected String[] names = { "Kilometers", "Meters", "Centimeters", "Millimeters", "Miles", "Yards", "Feet", "Inches" };
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            dList1.DataSource = names;
+            dList1.DataBind();
+            dList2.DataSource = names;
+            dList2.DataBind();
+        }
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        TextBox2.Text = "";
+        Label1.Text = "";
+        if (valid(TextBox1.Text) != true)
+        {
+            Label1.Text = "Error! Invalid Input! Valid value is NATURAL number!";
+        }
+        else
+        {
+            double tmp1 = Convert.ToDouble(TextBox1.Text);
+            if (tmp1 < 0)
+            {
+                Label1.Text = "Error! Invalid Input! Valid value is a positive number!";
+                return;
+
+            }
+            double[] ConvertParameter = { 0.001, 1, 100, 1000, 0.0006124, 1.0936133, 3.2808399, 39.3700787 };
+            double tmp2 = tmp1 / ConvertParameter[dList1.SelectedIndex];
+            double res = tmp2 * ConvertParameter[dList2.SelectedIndex];
+            TextBox2.Text = double.Parse(res.ToString()).ToString("0.##########");
+        }
+
+    }
+
+    public static bool valid(String input)
+    {
+        return (input != string.Empty && !Regex.IsMatch(input, "[^0-9]$")) ? true : false;
+    }
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+}
